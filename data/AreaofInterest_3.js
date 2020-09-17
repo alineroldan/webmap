55f3fc388de0888d5a2a4b23e61a1053c14362b6
alineroldan <71422852+alineroldan@users.noreply.github.com>
2020-09-17 11:29:34 +0700

    My first web map

---
+++ data/AreaofInterest_3.js
@@ -0,0 +1,8 @@
+var json_AreaofInterest_3 = {
+"type": "FeatureCollection",
+"name": "AreaofInterest_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 166.541563799829561, -16.258167776621612 ], [ 166.541563799829561, -14.644081341580616 ], [ 168.282871560074483, -14.644081341580616 ], [ 168.282871560074483, -16.258167776621612 ], [ 166.541563799829561, -16.258167776621612 ] ] ] ] } }
+]
+}
